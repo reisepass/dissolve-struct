b3f7aa763011965070406d488ae2c94de2efd624
reisepass <rubenwolff@gmail.com>
2015-04-29 17:34:35 +0200

    mod bash

--- dissolve-struct-examples/dissolveEc2Exp1.sh
+++ dissolve-struct-examples/dissolveEc2Exp1.sh
@@ -4,7 +4,7 @@ EXP_NAME=Ec2_x4_m2Large_Exp2
 logOut=${EXP_NAME}.log
 csvOut=${EXP_NAME}.csv
 
-baseCMD="/root/spark/bin/spark-submit --class ch.ethz.dalab.dissolve.examples.neighbourhood.runMSRC --jars /root/.ivy2/local/ch.ethz.dalab/dissolvestruct_2.10/0.1-SNAPSHOT/jars/dissolvestruct_2.10.jar,/root/.ivy2/cache/cc.factorie/factorie/jars/factorie-1.0.jar,/root/.ivy2/cache/com.github.scopt/scopt_2.10/jars/scopt_2.10-3.3.0.jar --driver-memory 2G     target/scala-2.10/dissolvestructexample_2.10-0.1-SNAPSHOT.jar -local=false"
+baseCMD="/root/spark/bin/spark-submit --class ch.ethz.dalab.dissolve.examples.neighbourhood.runMSRC --jars /root/.ivy2/local/ch.ethz.dalab/dissolvestruct_2.10/0.1-SNAPSHOT/jars/dissolvestruct_2.10.jar,/root/.ivy2/cache/cc.factorie/factorie/jars/factorie-1.0.jar,/root/.ivy2/cache/com.github.scopt/scopt_2.10/jars/scopt_2.10-3.3.0.jar --driver-memory 2G     target/scala-2.10/dissolvestructexample_2.10-0.1-SNAPSHOT.jar -local=false -debug=true"
     
 if ! [[ -e $csvOut ]] ; then
 	# " println("#RoundProgTag# , %s , %s , %.3f, %d, %f, %f, %f, %f, %f , %.2f, %s, %s"        .format(solverOptions.runName,solverOptions.gitVersion,elapsedTime, roundNum, dualityGap, primal,dual, trainError, testError,solverOptions.sampleFrac, if(solverOptions.doWeightedAveraging) "t" else "f", if(solverOptions.onlyUnary) "t" else "f"  )) "
